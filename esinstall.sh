c1140a6df65c99522271bb79c6951184ef54131c
ritasebastian <134355537+ritasebastian@users.noreply.github.com>
2024-11-14 12:35:11 -0800

    Update esinstall.sh

--- esinstall.sh
+++ esinstall.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
+# Create elasticsearch directories
+sudo mkdir -p /users/elasticsearch
+sudo mkidr -p /elasticsearch/product
+sudo mkidr -p /elasticsearch/data
+sudo mkidr -p /elasticsearch/config
+sudo mkdir -p /elasticsearch/log
+
 # Script used to setup Elasticsearch. Can be run as a regular user (needs sudo)
 
 ES_USER="elasticsearch"
 ES_GROUP="$ES_USER"
-ES_HOME="/usr/local/share/elasticsearch"
+HOME="/users/elasticsearch"
+ES_HOME="/elasticsearch/product"
+JAVA_HOME="/elasticsearch/product/java"
 ES_CLUSTER="clustername"
-ES_DATA_PATH="/var/data/elasticsearch"
-ES_LOG_PATH="/var/log/elasticsearch"
-ES_HEAP_SIZE="1g"
+ES_DATA_PATH="/elasticsearch/data"
+ES_LOG_PATH="/elasticsearch/log"
+CONFIG_PATH="/elasticsearch/config"
+sudo chown -R $ES_USER:$ES_GROUP $HOME $ES_HOME $ES_DATA_PATH $ES_LOG_PATH $CONFIG_PATH $JAVA_HOME
+ES_HEAP_SIZE="3g"
 ES_MAX_OPEN_FILES=32000
 
 # Path to main config
-CONFIG="/etc/elasticsearch/elasticsearch.yml"
+CONFIG="/elasticsearch/config/elasticsearch.yml"
 
 # Add group and user (without creating the homedir)
 echo "Add user: $ES_USER"
@@ -24,17 +35,15 @@ echo "$ES_USER hard nofile $ES_MAX_OPEN_FILES" | sudo tee -a /etc/security/limit
 echo "Update system"
 sudo yum update -y
 
-sudo mkdir -p $ES_HOME
 
-sudo chown -R $ES_USER:$ES_GROUP $ES_LOG_PATH $ES_HOME
 
 echo "Install OpenJDK 11"
 # Download and install Amazon Corretto 11 (OpenJDK)
-cd /usr/local/share/elasticsearch
-sudo wget https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.tar.gz
+
+wget https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.tar.gz
 
 # Extract the downloaded binary
-sudo tar zxvf amazon-corretto-11-x64-linux-jdk.tar.gz
+tar zxvf amazon-corretto-11-x64-linux-jdk.tar.gz
 
 # Rename the extracted directory to 'java'
 sudo mv amazon-corretto-11.* java
